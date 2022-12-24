d5c2bb88e484e7032fbeb3ac7db365e73a448fbe
ROMAN <robor55287@gmail.com>
2022-12-24 17:17:31 +0300

    comment

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,39 +1,74 @@
 import React, { useState } from "react";
 import api from "../api";
+
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
     const handleDelete = (userId) => {
         setUsers(users.filter((user) => user._id !== userId));
+        console.log(userId);
     };
     const renderPhrase = (number) => {
-        const lastOne = Number(number.toString().slice(-1));
-        if (number > 4 && number < 15) return "человек тусанет";
-        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
-        if (lastOne === 1) return "человек тусанет";
-        return "человек тусанет";
+        if (number > 4 && number < 15)
+            return `${number} человек тусанет с тобой сегодня`;
+        if (number >= 2 && number <= 4)
+            return `${number} человека тусанут с тобой сегодня`;
+        if (number === 1) return `${number} человек тусанет с тобой сегодня`;
+        return "Никто с тобой не тусанет";
     };
     return (
         <>
-            <h2>Сколько человек</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Профессия</th>
-                        <th scope="col">Встретился, раз</th>
-                        <th scope="col">Оценка</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                </tbody>
-            </table>
+            <h2>
+                <span
+                    className={
+                        "badge " +
+                        (users.length > 0 ? "bg-primary" : "bg-danger")
+                    }
+                >
+                    {renderPhrase(users.length)}
+                </span>
+            </h2>
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user._id}>
+                                <td>{user.name}</td>
+                                <td>
+                                    {user.qualities.map((item) => (
+                                        <span
+                                            className={
+                                                "badge m-1 bg-" + item.color
+                                            }
+                                        >
+                                            {item.name}
+                                        </span>
+                                    ))}
+                                </td>
+                                <td>{user.profession.name}</td>
+                                <td>{user.completedMeetings}</td>
+                                <td>{user.rate}/5</td>
+                                <td>
+                                    <button
+                                        onClick={() => handleDelete(user._id)}
+                                        className="btn btn-danger"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Users from "./components/users";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Users />);
+root.render(
+    <React.StrictMode>
+        <Users />
+    </React.StrictMode>
+);
 
 reportWebVitals();
